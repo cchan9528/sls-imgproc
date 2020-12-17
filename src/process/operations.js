adaa6e98294e198365ffebcec8e56b72efe975b2
Chris Chan <cchan9528@gmail.com>
2020-12-17 01:04:43 -0800

    Added basic logic for running exec on image after downloading from S3.

--- src/process/operations.js
+++ src/process/operations.js
@@ -30,12 +30,49 @@ function selectAlgorithm(x) {
     return algorithm;
 }
 
+async function downloadInputs(imgS3Path, localpath){
+    const s3client = new AWS.S3({
+        s3ForcePathStyle: true,
+        accessKeyId: process.env.s3accessKeyId,
+        secretAccessKey: process.env.s3secretAccessKey,
+        endpoint: new AWS.Endpoint('http://'+process.env.s3host+':'+process.env.s3port),
+    });
+    try {
+        let localpath = localpath ? localpath : '/local/inpath';
+        let res = await s3client.getObject({
+            Bucket: 'local-bucket',
+            Key: 'upload/123/456',
+            Body: new Buffer('cdasdsdf')
+        }).promise();
+        return {'path' : localpath};
+    } catch (err) {
+        return {'err' : 'failed downloading image from S3'};
+    }
+};
+
+async function runAlgorithm(algorithm, inpath, outpath) {
+    let outpath = outpath ? outpath : '/local/outpath'
+    let outfile = fs.open(outpath);
+    let res = null;
+    try{
+        let output = subprocess.exec(algorithm, inpath);
+        outfile.write(output);
+        res = {'output' : output};
+    } catch(err) {
+        res = {'err' : err};
+    } finally {
+        outfile.close();
+    }
+    return res;
+};
+
 async function processImage(options){
     try{
         let algorithm = selectAlgorithm(options.algorithm);
-        let output = await process( options.imgS3Path, algorithm );
+        let inpath = await downloadInputs(options.imgS3Path).path;
+        let outpath = await runAlgorithm( algorithm, inpath, outpath ).output;
         let s3Response = await upload({
-            'output' : output,
+            'output' : outpath,
             'uid' : options.uid
         });
     } catch (err) {
