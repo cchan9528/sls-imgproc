9efee014c07d89679770ca0bcaa7db3950f0045f
Chris Chan <cchan9528@gmail.com>
2021-02-24 22:52:11 -0800

    Minor cleanup

--- src/notify/event.js
+++ src/notify/event.js
@@ -12,22 +12,17 @@ async function handler(event, context) {
         }
         return userToInstances;
     }, {});
-    console.log("uploads");
-    console.log(uploads);
     try {
         //////////////////////////////////////////////////
         // Notify users upload(s) worked
         //////////////////////////////////////////////////
         let res = await Promise.all(Object.keys(uploads).map(function(user) {
-            console.log("sending to user");
-            console.log(user);
             let conn = `http://localhost:3001/@connections/${user}`;
             return axios.post(conn, {
                 'statusCode': 200,
                 'success': uploads[user]
             });
         }));
-        console.log(res);
     } catch (err) {
         //////////////////////////////////////////////////
         // To Be Implemented
--- tst/integration/upload/notify.test.js
+++ tst/integration/upload/notify.test.js
@@ -1,31 +1,29 @@
-'use strict';
-
 const Axios = require('axios');
 const FormData = require('form-data');
 const WebSocket = require('ws');
 
 test('Notify on S3 Upload', function(done) {
     let connected = false;
-    listenForNotifications(function(data) {
+    let ws = getServerlessOfflineClient();
+    ws.on('message', function(data) {
         const message = JSON.parse(data);
-        console.log(message);
         if (!connected) {
             connected = true;
-            uploadObjectToServerlessOffline(message.body);
+            uploadObjectToServerlessS3Local(message.body);
         } else {
+            // Add expectations here
             done();
         }
     });
 });
 
-function listenForNotifications(onMessage) {
+function getServerlessOfflineClient(onMessage) {
     const serverlessOffline = 'ws://localhost:3001';
     const ws = new WebSocket(serverlessOffline);
-    ws.on('message', onMessage);
     return ws;
 }
 
-async function uploadObjectToServerlessOffline(presignedInfo) {
+async function uploadObjectToServerlessS3Local(presignedInfo) {
     let formData = new FormData();
     Object.entries(presignedInfo.fields).forEach(function(entry) {
         const [field, value] = entry;
